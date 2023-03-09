6993a0201b7f012eaec2ae8c8e61ec5b658453d6
brady-619 <driftting-king@hotmail.com>
2023-03-09 14:35:51 -0600

    falta servicio update

--- src/app/pages/editar-beneficiarios/editar-beneficiarios.page.ts
+++ src/app/pages/editar-beneficiarios/editar-beneficiarios.page.ts
@@ -12,16 +12,22 @@ import { GetListdoIdService }  from '../../services/get-listdo-id.service';
 })
 export class EditarBeneficiariosPage implements OnInit {
 
+  nombre:any;
+  rfc:any;
+  telefono:any;
+  cuenta:any;
+  banco:any;
+
 
   constructor(private getlistadoid: GetListdoIdService,private router: Router,private insertar: InsertBeneficiarioService, private alertController: AlertController) { }
 
-  beneficiarioForm = new FormGroup({
-    nombre: new FormControl('', [Validators.required, Validators.minLength(1)]),
-    rfc: new FormControl(''),
-    telefono: new FormControl('', [Validators.minLength(10), Validators.pattern(/^-?(0|[1-9]\d*)?$/)]),
-    cuenta: new FormControl('', Validators.minLength(10)),
-    banco: new FormControl('')
-  });
+  // beneficiarioForm = new FormGroup({
+  //   nombre: new FormControl('', [Validators.required, Validators.minLength(1)]),
+  //   rfc: new FormControl(''),
+  //   telefono: new FormControl('', [Validators.minLength(10), Validators.pattern(/^-?(0|[1-9]\d*)?$/)]),
+  //   cuenta: new FormControl('', Validators.minLength(10)),
+  //   banco: new FormControl('')
+  // });
 
 
   data:any;
@@ -32,7 +38,9 @@ export class EditarBeneficiariosPage implements OnInit {
 
 
 /*render*/
-  async ngAfterViewInit() {
+  async ionViewWillEnter() {
+
+    
 
 
 
@@ -42,16 +50,22 @@ export class EditarBeneficiariosPage implements OnInit {
     const params ={nombre: localStorage.getItem("nombre_beneficiario")};
 
     //  const params = this.beneficiarioForm.value
-    console.log("antes", this.beneficiarioForm.value);
+    // console.log("antes", this.beneficiarioForm.value);
 
 
      await this.getlistadoid.ListadoBeneficiarioId(params).then(async respuesta => {
        console.log(respuesta);
 
-       
+       this.data= respuesta.data
 
+       console.log(this.data[0].nombre);
 
-
+     
+       this.nombre=this.data[0].nombre;
+       this.rfc=this.data[0].rfc;
+       this.telefono=this.data[0].telefono;
+       this.cuenta=this.data[0].cuenta;
+       this.banco=this.data[0].banco;
 
 
        this.data= respuesta.data
@@ -72,75 +86,164 @@ export class EditarBeneficiariosPage implements OnInit {
 
   }
 
+  
+ async save(nombre:any,rfc:any, telefono:any, cuenta:any,banco:any){
+  console.log(nombre, rfc ,telefono, cuenta, banco);
+// edits
 
 
 
-
-
-
-
-
-
-
-  get registerFormControl() {
-    return this.beneficiarioForm.controls;
+  let data2 = {
+    //    data:[{nombre: nombre , dueno: dueno, apellido: apellido,telefono1: tel1, telefono2:tel2, domicilio:domicilio, email:email, giro,vendedor: this.nombre_global, latitud: this.latitud, longitud: this.longitud }]
+    data: [{nombre: nombre, rfc: rfc, telefono: telefono, cuenta: cuenta, banco:banco  }]
   }
-  async guardar() {
 
-    const params = this.beneficiarioForm.value
-    console.log("guardar", params);
+  console.log(data2)
 
 
+  await this.insertar.InsertBeneficiario(data2).then(async respuesta => {
+    console.log(respuesta);
 
-    let data2 = {
-      //    data:[{nombre: nombre , dueno: dueno, apellido: apellido,telefono1: tel1, telefono2:tel2, domicilio:domicilio, email:email, giro,vendedor: this.nombre_global, latitud: this.latitud, longitud: this.longitud }]
-      data: [params]
+    if (respuesta.status = "000") {
+      const alert = await this.alertController.create({
+        header: 'Alert',
+        subHeader: 'Tu registro ha sido exitoso',
+        // message: 'This is an alert!',
+        buttons: ['OK'],
+      });
+
+    
+await alert.present();
+      
+  // window.location.reload();
+  setTimeout(function(){location.reload()}, 3000);
+
+
+
+      let data2 = {
+        //    data:[{nombre: nombre , dueno: dueno, apellido: apellido,telefono1: tel1, telefono2:tel2, domicilio:domicilio, email:email, giro,vendedor: this.nombre_global, latitud: this.latitud, longitud: this.longitud }]
+        data: [{nombre: "", rfc: "", telefono: "", cuenta: "", banco:""  }]
+      }
+
+      // this.beneficiarioForm = new FormGroup({
+      //   nombre: new FormControl('', [Validators.required, Validators.minLength(1)]),
+      //   rfc: new FormControl(''),
+      //   telefono: new FormControl(''),
+      //   cuenta: new FormControl(''),
+      //   banco: new FormControl('')
+      // });
+
+      
+    // this.router.navigate(['beneficiarios']);
+
+
+
+
+    } else {
+      const alert = await this.alertController.create({
+        header: 'Alert',
+        subHeader: 'Error al cargar tu registro',
+        // message: 'This is an alert!',
+        buttons: ['OK'],
+      });
+
+      await alert.present();
+
     }
 
 
-    await this.insertar.InsertBeneficiario(data2).then(async respuesta => {
-      console.log(respuesta);
+  })
 
-      if (respuesta.status = "000") {
-        const alert = await this.alertController.create({
-          header: 'Alert',
-          subHeader: 'Tu registro ha sido exitoso',
-          // message: 'This is an alert!',
-          buttons: ['OK'],
-        });
 
-        await alert.present();
 
-        // this.beneficiarioForm = new FormGroup({
-        //   nombre: new FormControl('', [Validators.required, Validators.minLength(1)]),
-        //   rfc: new FormControl(''),
-        //   telefono: new FormControl(''),
-        //   cuenta: new FormControl(''),
-        //   banco: new FormControl('')
-        // });
+
+
+  
+
+}
+
+
+
+
+
+
+
+
+
+
+
+  // get registerFormControl() {
+  //   return this.beneficiarioForm.controls;
+  // }
+
+  
+  // async guardar() {
+
+  //   const params = this.beneficiarioForm.value
+  //   console.log("guardar", params);
+
+
+
+  //   let data2 = {
+  //     //    data:[{nombre: nombre , dueno: dueno, apellido: apellido,telefono1: tel1, telefono2:tel2, domicilio:domicilio, email:email, giro,vendedor: this.nombre_global, latitud: this.latitud, longitud: this.longitud }]
+  //     data: [params]
+  //   }
+
+
+  //   await this.insertar.InsertBeneficiario(data2).then(async respuesta => {
+  //     console.log(respuesta);
+
+  //     if (respuesta.status = "000") {
+  //       const alert = await this.alertController.create({
+  //         header: 'Alert',
+  //         subHeader: 'Tu registro ha sido exitoso',
+  //         // message: 'This is an alert!',
+  //         buttons: ['OK'],
+  //       });
+
+  //       await alert.present();
+
+  //       // this.beneficiarioForm = new FormGroup({
+  //       //   nombre: new FormControl('', [Validators.required, Validators.minLength(1)]),
+  //       //   rfc: new FormControl(''),
+  //       //   telefono: new FormControl(''),
+  //       //   cuenta: new FormControl(''),
+  //       //   banco: new FormControl('')
+  //       // });
 
         
-      this.router.navigate(['beneficiarios']);
+  //     this.router.navigate(['beneficiarios']);
 
 
 
 
-      } else {
-        const alert = await this.alertController.create({
-          header: 'Alert',
-          subHeader: 'Error al cargar tu registro',
-          // message: 'This is an alert!',
-          buttons: ['OK'],
-        });
+  //     } else {
+  //       const alert = await this.alertController.create({
+  //         header: 'Alert',
+  //         subHeader: 'Error al cargar tu registro',
+  //         // message: 'This is an alert!',
+  //         buttons: ['OK'],
+  //       });
 
-        await alert.present();
+  //       await alert.present();
 
-      }
+  //     }
 
 
-    })
+  //   })
 
-  }
+  // }
+
+
+
+
+
+
+
+
+
+
+
 
 
 }
